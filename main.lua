3b5917377f9493602c92b47609382ad266d96a60
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2021-12-18 20:32:45 +0000

    Test

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
 local daimonMaincra = RegisterMod("Daimon Shotgunner and Mordhau",1)
+
+function daimonMaincra:onTear(tear)
+    local player = Isaac.GetPlayer(0) --get the player entity
+    player:AddCoins(1) -- add a coin
+end
+
+daimonMaincra:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR , daimonMaincra.onTear)
